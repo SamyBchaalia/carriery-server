58e217971ee75ebb4039bd7a0146580942d293e5
halim-boussada <66827428+halim-boussada@users.noreply.github.com>
2021-11-05 12:07:29 +0100

    Update reservationRouter.js

--- server/router/reservationRouter.js
+++ server/router/reservationRouter.js
@@ -6,6 +6,7 @@ router.put("/confirmP/:id", reservationController.updatepayment);
 router.put("/feedback/:id", reservationController.updatefeedback);
 router.put("/status/:id", reservationController.updatestatus);
 router.get("/", reservationController.get);
+router.get("/archieve", reservationController.getarchive);
 router.get("/coach/:coachId", reservationController.getreservationBycoachId);
 router.get("/user/:userId", reservationController.getreservationByuserId);
 router.get("/field/:field", reservationController.getreservationByfield);
